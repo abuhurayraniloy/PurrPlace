110374ba825dcfb36601725d8ffaca7d0d7a7134
botfigther02 <abuhurayraniloy02@gmail.com>
2025-03-22 00:36:31 +0600

    Fixing the duplicate registration issue

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -3,19 +3,26 @@ import prisma from "../lib/prisma.js"
 export const register = async (req, res)=>{
     const { username, email, password } = req.body;
 
-    //Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
-    console.log(hashedPassword);
+    try{
+      //Hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
+      console.log(hashedPassword);
 
-    // CREATE A NEW USER AND SAVE TO DB
-    const newUser = await prisma.user.create({
-      data: {
-        username,
-        email,
-        password: hashedPassword,
-      },
-    });
-    console.log(newUser);
+      // CREATE A NEW USER AND SAVE TO DB
+      const newUser = await prisma.user.create({
+        data: {
+          username,
+          email,
+          password: hashedPassword,
+        },
+      });
+      console.log(newUser);
+      res.status(201).json({message: "User created successfully!"});
+    }
+    catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Failed to create user!" });
+    }
 };
 
 export const login = (req, res)=>{
